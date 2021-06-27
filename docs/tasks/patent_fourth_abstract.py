669dfe3ec1d1c842c6a15d7b5e9b2c3e14e0e9ef
outdreamer <outdreamer@users.noreply.github.com>
2021-06-27 10:14:09 -0700

    organize

--- docs/tasks/patent_fourth_abstract.py
+++
@@ -1,7 +0,0 @@
-Inventor Name: Joni Jezewski
-Filing Date: 06/23/2021
-
-ABSTRACT 
-
-Solution automation & interface analysis components can be implemented in many ways, such as by specifying input/outputs & training a learning (generate, test & update) algorithm on the input/output data to generate a prediction function, to replace logic connecting input & outputs. Alternatively, specific logic implementations to connect input/ output of sub-tasks to implement solution automation & interface analysis are included in the specification of this invention.
-
