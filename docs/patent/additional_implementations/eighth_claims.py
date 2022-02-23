d6005d2f28b8c87e7e1745474068467a6adc518f
outdreamer <outdreamer@users.noreply.github.com>
2022-02-23 00:44:39 -0800

    add update

---
+++ docs/patent/additional_implementations/eighth_claims.py
@@ -0,0 +1,57 @@
+Inventor Name: Joni Jezewski
+Filing Date: 02/22/2022
+Invention Title: Additional Solution Automation & Interface Analysis Implementations & Applications
+
+
+CLAIMS
+
+1. A method optionally comprising relating solution automation & interface analysis implementation variables & components such as the following:
+- Problem-solving intents like 'apply generally successful solution or optimization structures'
+- Solution automation workflows & their useful structures (like 'interchangeable alternate variable sets to generate a workflow') & their implementation variables (like 'variable workflow component combinations')
+- Variables of implementations of solution automation & interface analysis (like 'specific structures, like certainty structures, applied in a workflow as limits/bases, such as known suboptimal workflows to apply as a base workflow')
+- Generally useful structures like 'structure/intent connections' (for example, like 'combinations of uncertainty/certainty structures' & its associated intents like 'resolving uncertainties, based on certainties applied as a foundation')
+- Error/sub-optimality structures (error structures such as 'differences unresolvable with adjacent interaction level concepts/functions')
+
+2. The method of claim 1, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'problem-solving intents' like 'apply structures with a reason to be successful, like having functionality such as fulfilling a problem-solving intent'.
+
+3. The method of claim 1, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'variable workflow component combinations' like 'useful structures and a problem-solving intent' or 'useful structures and a problem-solving intent and a solution metric fulfilled by that structure-intent interaction'.
+
+4. The method of claim 1, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'specific structures required to be applied in a workflow' like'certainty structures applied in a workflow as limits/bases, such as known suboptimal workflows to apply as a base workflow'.
+
+5. The method of claim 1, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'useful structures' like 'structure-intent connections' and 'interchangeable alternate variable sets (requirements, format sequences, etc) to generate a workflow'.
+
+6. The method of claim 1, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'error structures corrected/created by a workflow/query' like 'differences unresolvable with adjacent interaction level concepts/functions'.
+
+7. A non-transitory computer-readable medium containing instructions that, when executed by a processor, cause a device to perform operations, the operations comprising relating solution automation & interface analysis implementation variables & components such as the following:
+- Problem-solving intents like 'apply generally successful solution or optimization structures'
+- Solution automation workflows & their useful structures (like 'interchangeable alternate workflows') & their implementation variables (like 'variable workflow component combinations')
+- Generally useful structures like 'structure/intent connections' (for example, like 'combinations of uncertainty/certainty structures' & its associated intents like 'resolving uncertainties, based on certainties applied as a foundation')
+- Variables of implementations of solution automation & interface analysis (like 'certainty structures applied in a workflow as limits/bases, such as known suboptimal workflows to apply as a base workflow')
+- Error/sub-optimality structures (error structures such as 'differences unresolvable with adjacent interaction level concepts/functions')
+
+8. The non-transitory computer-readable medium of claim 7, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'problem-solving intents' like 'apply structures with a reason to be successful, like having functionality such as fulfilling a problem-solving intent'.
+
+9. The non-transitory computer-readable medium of claim 7, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'variable workflow component combinations' like 'useful structures and a problem-solving intent' or 'useful structures and a problem-solving intent and a solution metric fulfilled by that structure-intent interaction'.
+
+10. The non-transitory computer-readable medium of claim 7, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'specific structures required to be applied in a workflow' like'certainty structures applied in a workflow as limits/bases, such as known suboptimal workflows to apply as a base workflow'.
+
+11. The non-transitory computer-readable medium of claim 7, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'useful structures' like 'structure-intent connections' and 'interchangeable alternate variable sets (requirements, format sequences, etc) to generate a workflow'.
+
+12. The non-transitory computer-readable medium of claim 7, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'error structures corrected/created by a workflow/query' like 'differences unresolvable with adjacent interaction level concepts/functions'.
+
+13. A system comprising: one or more processors; and one or more non-transitory computer-readable media containing instructions that, when executed by the one or more processors, cause the system to perform operations, the operations comprising relating solution automation & interface analysis implementation variables & components such as the following:
+- Problem-solving intents like 'apply generally successful solution or optimization structures'
+- Solution automation workflows & their useful structures (like 'interchangeable alternate workflows') & their implementation variables (like 'variable workflow component combinations')
+- Generally useful structures like 'structure/intent connections' (for example, like 'combinations of uncertainty/certainty structures' & its associated intents like 'resolving uncertainties, based on certainties applied as a foundation')
+- Variables of implementations of solution automation & interface analysis (like 'certainty structures applied in a workflow as limits/bases, such as known suboptimal workflows to apply as a base workflow')
+- Error/sub-optimality structures (error structures such as 'differences unresolvable with adjacent interaction level concepts/functions')
+
+14. The system of claim 13, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'problem-solving intents' like 'apply structures with a reason to be successful, like having functionality such as fulfilling a problem-solving intent'.
+
+15. The system of claim 13, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'variable workflow component combinations' like 'useful structures and a problem-solving intent' or 'useful structures and a problem-solving intent and a solution metric fulfilled by that structure-intent interaction'.
+
+16. The system of claim 13, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'specific structures required to be applied in a workflow' like'certainty structures applied in a workflow as limits/bases, such as known suboptimal workflows to apply as a base workflow'.
+
+17. The system of claim 13, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'useful structures' like 'structure-intent connections' and 'interchangeable alternate variable sets (requirements, format sequences, etc) to generate a workflow'.
+
+18. The system of claim 13, wherein example component variables of solution automation & interface analysis (like 'solution automation workflow variables') include 'error structures corrected/created by a workflow/query' like 'differences unresolvable with adjacent interaction level concepts/functions'.
