ced444f8257aed34333eaacdebba80230f03d286
Joni Jezewski <jonijezewski@Jonis-Air.lan>
2025-11-07 06:33:55 -0500

    update

--- find_existing_solutions/deploy/auto_deploy/aws_elk_config.tf
+++ find_existing_solutions/deploy/auto_deploy/aws_elk_config.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "task_ec2_instance" {
   ami		   = "ami-024d1b90da07e64a6"
   instance_type = "t2.micro"
-  user_data	 = file("/Users/jjezewski/Documents/build_a_cure/tasks/install_boot_elk.sh")
+  user_data	 = file("/Users/jjezewski/Documents/solver/tasks/install_boot_elk.sh")
   tags = {
     Name  = "task_ec2_instance_elk"
   }
